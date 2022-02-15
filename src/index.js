da734a3629ff036d7977476c245b4d2728837ce0
Ben Daniels <benbanj@hotmail.com>
2022-02-15 17:51:24 +0000

    Adding ability to add a Todo to a Project
    - Creating a #todos attribute to hold all the todos assigned to a
    project
    - Setting a getter for the #todos function
    - Creating an addTodo function for Project constructor

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import Project from './project.js';
 import Todo from './todo.js';
 
 let obj = {title: "A title", description: "A description", dueDate: 'tomoroow', priority: 'High'}
-let foo = new Todo(obj)
-console.log(foo.description)
+let todo = new Todo(obj)
+console.log(todo.description)
 
-let pop = new Project('A project title')
-console.log(pop.title)
+let project = new Project('A project title')
+project.addTodo(todo)
+console.log(project.todos)
 
--- src/project.js
+++ src/project.js
@@ -1,14 +1,29 @@
+import Todo from './todo.js';
+
 function Project(title) {
   this._title = title;
+  this._todos = [];
 }
 
-Object.defineProperty(Project.prototype, 'title', {
-  get: function() {
-    return this._title;
+Object.defineProperties(Project.prototype, {
+  title: {
+    get: function() {
+      return this._title;
+    },
+    set: function(title) {
+      this._title = title;
+    }
   },
-  set: function(title) {
-    this._title = title;
+  todos: {
+    get: function() {
+      return this._todos;
+    }
   }
 });
+
+Project.prototype.addTodo = function(todo) {
+  if (!(todo instanceof Todo)) throw "Can only add an instance of Todo to a project";
+  this._todos.push(todo);
+}
 
 export default Project;
