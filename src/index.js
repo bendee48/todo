194ae093089506623d2bd8f834067e19a18eb247
Ben Daniels <benbanj@hotmail.com>
2022-08-30 18:36:22 +0100

    Adding Eslint
    - Adding eslint to project
    - Making linting changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,43 @@
-import Project from './project.js';
-import Todo from './todo.js';
-import projectsPage from './projectsPage.js';
-import eventObserver from './eventObserver.js';
-import modal from './modal.js';
-import projectContent from './projectContent.js';
+import Project from './project';
+import Todo from './todo';
+import projectsPage from './projectsPage';
+import eventObserver from './eventObserver';
+import modal from './modal';
+import projectContent from './projectContent';
 
 // Sample Data - Creating a todo
-let obj = {title: "Buy new suitcase", description: "A description", dueDate: '2022-10-08', priority: 'med'}
-let obj1 = {title: "Pick up travel docs", description: "", dueDate: '2022-10-10', priority: 'high'}
-let obj2 = {title: "Buy sunglasses", description: "Use voucher from birthday", dueDate: '2022-10-19', priority: 'low'}
-let todo = new Todo(obj)
-let todo1 = new Todo(obj1)
-let todo2 = new Todo(obj2)
+const obj = {
+  title: 'Buy new suitcase', description: 'A description', dueDate: '2022-10-08', priority: 'med',
+};
+const obj1 = {
+  title: 'Pick up travel docs', description: '', dueDate: '2022-10-10', priority: 'high',
+};
+const obj2 = {
+  title: 'Buy sunglasses', description: 'Use voucher from birthday', dueDate: '2022-10-19', priority: 'low',
+};
+const todo = new Todo(obj);
+const todo1 = new Todo(obj1);
+const todo2 = new Todo(obj2);
 
 // Sample Data - Creating projects/ adding todos to project
-let project = Project.create('Example')
-project.addTodo(todo)
-project.addTodo(todo1)
-project.addTodo(todo2)
+const project = Project.create('Example');
+project.addTodo(todo);
+project.addTodo(todo1);
+project.addTodo(todo2);
 
 // Display projects
 Project.loadLocal(); // Load any saved projects
-let projects = Project.all;
+const projects = Project.all;
 projectsPage.displayProjects(projects);
 
 // Event Observers
 // Update Project display on creation of a new Project
-eventObserver.subscribe({subName: "Display Projects", funcToCall: projectsPage.displayProjects});
+eventObserver.subscribe({ subName: 'Display Projects', funcToCall: projectsPage.displayProjects });
 // Create and Save a Project
-eventObserver.subscribe({subName: "Create Project", funcToCall: Project.create});
+eventObserver.subscribe({ subName: 'Create Project', funcToCall: Project.create });
 // Close Modal
-eventObserver.subscribe({subName: "Close Modal", funcToCall: modal.closeModal})
+eventObserver.subscribe({ subName: 'Close Modal', funcToCall: modal.closeModal });
 // Update Todos
-eventObserver.subscribe({subName: "Update Todos", funcToCall: projectContent.updateTodos})
+eventObserver.subscribe({ subName: 'Update Todos', funcToCall: projectContent.updateTodos });
 // Save Projects to local
-eventObserver.subscribe({subName: "Save Projects", funcToCall: Project.saveLocal})
-
+eventObserver.subscribe({ subName: 'Save Projects', funcToCall: Project.saveLocal });
